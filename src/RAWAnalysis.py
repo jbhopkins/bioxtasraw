Changes: - Fixed a bug in how the error bars for log(I) were calculated in autorg. - Switched autorg to calculate the guinier fit without error bar weighting, to match the guinier panel - Added possibility to use error bars to calculate fit in guinier panel (currently commented out).

src/RAWAnalysis.py
@@ -35,6 +35,7 @@ from scipy import polyval, polyfit, sqrt, integrate
 import scipy.interpolate as interp
 from scipy.constants import Avogadro
 import scipy.stats as stats
+import scipy.optimize
 
 import RAW, RAWSettings, RAWCustomCtrl, SASCalc, RAWPlot, SASFileIO, SASM, SASExceptions, BIFT, RAWGlobals
 
@@ -104,27 +105,39 @@ class GuinierPlotPanel(wx.Panel):
 
         self.canvas.mpl_disconnect(self.cid)
         
-        self.updateDataPlot(self.orig_i, self.orig_q, self.xlim)
+        self.updateDataPlot(self.orig_i, self.orig_q, self.orig_err, self.xlim)
 
         self.cid = self.canvas.mpl_connect('draw_event', self.ax_redraw)
         
     def _calcFit(self):
         ''' calculate fit and statistics '''
- 
         q_roi = self.q
         i_roi = self.i
+        err_roi = self.err
         
         x = np.power(q_roi, 2)
         y = np.log(i_roi)
+        err = y*np.absolute(err_roi/i_roi)
         
         #Remove NaN and Inf values:
         x = x[np.where(np.isnan(y) == False)]
+        err = err[np.where(np.isnan(y) == False)]
         y = y[np.where(np.isnan(y) == False)]
+
         x = x[np.where(np.isinf(y) == False)]
+        err = err[np.where(np.isinf(y) == False)]
         y = y[np.where(np.isinf(y) == False)]
+
         
         #Get 1.st order fit:
         ar, br = polyfit(x, y, 1)
+
+        #This uses error weighted points to calculate the Rg. Probably the correct way to do it, but different
+        #from how it has always been done.
+        # f = lambda x, a, b: a+b*x
+        # opt, cov = scipy.optimize.curve_fit(f, x, y, sigma = err, absolute_sigma = True)
+        # ar = opt[1]
+        # br = opt[0]
         
         #Obtain fit values:
         y_fit = polyval([ar, br], x)
@@ -168,23 +181,25 @@ class GuinierPlotPanel(wx.Panel):
         
         #Disconnect draw_event to avoid ax_redraw on self.canvas.draw()
         self.canvas.mpl_disconnect(self.cid)
-        self.updateDataPlot(ExpObj.i, ExpObj.q, xlim)
+        self.updateDataPlot(ExpObj.i, ExpObj.q, ExpObj.err, xlim)
         
         #Reconnect draw_event
         self.cid = self.canvas.mpl_connect('draw_event', self.ax_redraw)
 
-    def updateDataPlot(self, i, q, xlim):
-            
+    def updateDataPlot(self, i, q, err, xlim):
+
         xmin, xmax = xlim
         
         #Save for resizing:
         self.orig_i = i
         self.orig_q = q
+        self.orig_err = err
         self.xlim = xlim
         
         #Cut out region of interest
         self.i = i[xmin:xmax+1]
         self.q = q[xmin:xmax+1]
+        self.err = err[xmin:xmax+1]
         
         ## Plot the (at most) 3 first and last points after fit:
         if xmin < 20:
@@ -744,11 +759,12 @@ class GuinierControlPanel(wx.Panel):
         
         x = self.ExpObj.q
         y = self.ExpObj.i
+        err = self.ExpObj.err
         
         xlim = [i,i2]
 
         plotpanel.canvas.mpl_disconnect(plotpanel.cid) #disconnect draw event to avoid recursions
-        plotpanel.updateDataPlot(y, x, xlim)
+        plotpanel.updateDataPlot(y, x, err, xlim)
         plotpanel.cid = plotpanel.canvas.mpl_connect('draw_event', plotpanel.ax_redraw) #Reconnect draw_event
 
         
@@ -839,7 +855,6 @@ class GuinierFrame(wx.Frame):
         self.Layout()
 
         if self.GetBestSize()[0] > self.GetSize()[0] or self.GetBestSize()[1] > self.GetSize()[1]:
-            print 'fitting the guinier window size'
             splitter1.Fit()
             if platform.system() == 'Linux' and int(wx.__version__.split('.')[0]) >= 3:
                 size = self.GetSize()

src/SASCalc.py
@@ -74,8 +74,8 @@ def autoRg(sasm):
 
     #Start out by transforming as usual.
     qs = np.square(q)
-    il = np.log(np.absolute(i))
-    iler = np.log(err)
+    il = np.log(i)
+    iler = il*np.absolute(err/i)
 
     #Pick a minimum fitting window size. 10 is consistent with atsas autorg.
     min_window = 10
@@ -104,7 +104,22 @@ def autoRg(sasm):
     #We keep the fit.
     for w in window_list:
         for start in range(data_start,data_end-w, data_step):
-            opt, cov = scipy.optimize.curve_fit(f, qs[start:start+w], il[start:start+w], sigma = iler[start:start+w])
+            x = qs[start:start+w]
+            y = il[start:start+w]
+            yerr = iler[start:start+w]
+
+            #Remove NaN and Inf values:
+            x = x[np.where(np.isnan(y) == False)]
+            yerr = yerr[np.where(np.isnan(y) == False)]
+            y = y[np.where(np.isnan(y) == False)]
+
+            x = x[np.where(np.isinf(y) == False)]
+            yerr = yerr[np.where(np.isinf(y) == False)]
+            y = y[np.where(np.isinf(y) == False)]
+
+            # opt, cov = scipy.optimize.curve_fit(f, x, y, sigma = yerr, absolute_sigma = True)
+
+            opt, cov = scipy.optimize.curve_fit(f, x, y)
 
             if opt[1] < 0 and np.isreal(opt[1]) and np.isreal(opt[0]):
                 RG=np.sqrt(-3.*opt[1])
@@ -114,7 +129,7 @@ def autoRg(sasm):
 
                     #error in rg and i0 is calculated by noting that q(x)+/-Dq has Dq=abs(dq/dx)Dx, where q(x) is your function you're using 
                     #on the quantity x+/-Dx, with Dq and Dx as the uncertainties and dq/dx the derviative of q with respect to x.
-                    RGer=np.absolute(0.5*(np.sqrt(-3/opt[1])))*np.sqrt(np.absolute(cov[1,1,]))
+                    RGer=np.absolute(0.5*(np.sqrt(-3./opt[1])))*np.sqrt(np.absolute(cov[1,1,]))
                     I0er=I0*np.sqrt(np.absolute(cov[0,0]))
 
                     if RGer/RG <= 1:
@@ -212,16 +227,16 @@ def autoRg(sasm):
                 rger = fit_list[:,5][quality>quality[idx]-.1].std()
                 i0 = fit_list[:,6][quality>quality[idx]-.1].mean()
                 i0er = fit_list[:,7][quality>quality[idx]-.1].std()
-                idx_min = fit_list[idx,0]
-                idx_max = fit_list[idx,0]+fit_list[idx,1]-1
+                idx_min = int(fit_list[idx,0])
+                idx_max = int(fit_list[idx,0]+fit_list[idx,1]-1)
             except:
                 idx = quality.argmax()
                 rg = fit_list[idx,4]
                 rger1 = fit_list[idx,5]
                 i0 = fit_list[idx,6]
                 i0er = fit_list[idx,7]
-                idx_min = fit_list[idx,0]
-                idx_max = fit_list[idx,0]+fit_list[idx,1]-1
+                idx_min = int(fit_list[idx,0])
+                idx_max = int(fit_list[idx,0]+fit_list[idx,1]-1)
 
 
         else: