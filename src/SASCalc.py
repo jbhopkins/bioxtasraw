Changes: - Fixed a bug where the Porod volume calculation was not getting properly interpolated to q=0 - Fixed a bug where hitting the next/previous image buttons in the RAW Image plot would throw an error and crash RAW if the image currently displayed wasn't in the current working directory of the Files panel.

src/SASCalc.py
@@ -322,11 +322,11 @@ def porodVolume(sasm, rg, i0, start = 0, stop = -1, interp = False):
 
     if interp:
         def f(x):
-            i0*np.exp((-1./3.)*np.square(rg)*np.square(x))
+            return i0*np.exp((-1./3.)*np.square(rg)*np.square(x))
 
-        q_interp = np.arange(0,q_exp[0],100)
+        q_interp = np.linspace(0,q_exp[0]-.00001,100)
 
-        i_interp = f(x)
+        i_interp = f(q_interp)
 
         q = np.concatenate((q_interp,q_exp))
         i = np.concatenate((i_interp,i_exp))