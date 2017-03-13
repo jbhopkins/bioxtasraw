d8388de70f4086de8f34f5517cd980e2931f68fe
Jesse Hopkins <hopkins.jesse@gmail.com>
2017-03-13 14:17:46 +0000

    Updated the pyinstaller pyfai-calibrant hook to work with the new version of pyfai (changed where the calibrant resource directory is located).

--- src/WinLib/installer/hook-pyFAI.calibrant.py
+++ src/WinLib/installer/hook-pyFAI.calibrant.py
@@ -1,5 +1,5 @@
 from PyInstaller.utils.hooks import collect_data_files
 
-calibration_files = collect_data_files('pyFAI', subdir = 'calibration')
+calibration_files = collect_data_files('pyFAI', subdir = 'resources\\calibration')
 
 datas = calibration_files
