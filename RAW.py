Fixed an off by one error in sec autoupdate, minor fixes: - Fixed a bug where when updating a SEC plot for new frames with rg calculated, with the frame number didn't start at 0 it would throw an error. - Fixed a bug where the math package wasn't imported in sascalc (somehow). - Updated the version number. - Changed menu options in IFT control panel.

RAW.py
@@ -2995,11 +2995,13 @@ class MainWorkerThread(threading.Thread):
                 wx.CallAfter(self.main_frame.closeBusyDialog)
                 return
 
+        largest_frame = secm.plot_frame_list[-1]
+
         secm.append(filename_list, sasm_list, frame_list)
 
 
         if secm.calc_has_data:
-            self._updateCalcSECParams(secm, frame_list)
+            self._updateCalcSECParams(secm, range(len(frame_list))+largest_frame+1)
 
         self._updateSECMPlot(secm)
 
@@ -3226,7 +3228,7 @@ class MainWorkerThread(threading.Thread):
 
 
     def _updateCalcSECParams(self, secm, frame_list):
-
+        print frame_list
         molecule = secm.mol_type
 
         if molecule == 'Protein':
@@ -8492,15 +8494,15 @@ class IFTItemPanel(wx.Panel):
         if self.is_gnom:
             if opsys == 'Windows':
                 if os.path.exists(os.path.join(self.raw_settings.get('ATSASDir'), 'dammif.exe')):
-                    menu.Append(23, 'Run DAMMIF/N')
+                    menu.Append(23, 'Bead Model (DAMMIF/N)')
                 if os.path.exists(os.path.join(self.raw_settings.get('ATSASDir'), 'ambimeter.exe')):
-                    menu.Append(24, 'Run AMBIMETER')
+                    menu.Append(24, 'AMBIMETER')
             else:
                 if os.path.exists(os.path.join(self.raw_settings.get('ATSASDir'), 'dammif')):
-                    menu.Append(23, 'Run DAMMIF/N')
+                    menu.Append(23, 'Bead Model (DAMMIF/N)')
                 if os.path.exists(os.path.join(self.raw_settings.get('ATSASDir'), 'ambimeter')):
-                    menu.Append(24, 'Run AMBIMETER')
-        menu.Append(28, 'Run DENSS')
+                    menu.Append(24, 'AMBIMETER')
+        # menu.Append(28, 'Electron Density (DENSS)')
         menu.Append(25, 'SVD')
         menu.Append(26, 'EFA')
         menu.Append(27, 'Similarity Test')
@@ -10482,6 +10484,7 @@ class SECControlPanel(wx.Panel):
             self.initial_buffer_frame < self.final_buffer_frame):
             sasm_list = secm.getSASMList(self.initial_buffer_frame,
                 self.final_buffer_frame)
+            print sasm_list
             ref_sasm = sasm_list[0]
             qi_ref, qf_ref = ref_sasm.getQrange()
             pvals = np.ones(len(sasm_list[1:]), dtype=float)

RAWPlot.py
@@ -4058,7 +4058,7 @@ class SECPlotPanel(wx.Panel):
                 return
 
             if self.plotparams.get('x_axis_display') == 'frame':
-                xdata = secm.frame_list
+                xdata = secm.plot_frame_list
 
             elif self.plotparams.get('x_axis_display') == 'time':
                 time= secm.getTime()
@@ -4066,15 +4066,15 @@ class SECPlotPanel(wx.Panel):
                 if len(time) == 0:
                     wx.CallAfter(wx.MessageBox, "Time data not available for this data set. Reverting to frame number.", style=wx.ICON_ERROR | wx.OK)
                     self.plotparams['x_axis_display'] = 'frame'
-                    xdata = secm.frame_list
+                    xdata = secm.plot_frame_list
                 elif time[0] == -1:
                     wx.CallAfter(wx.MessageBox, "Time data not available for this data set. Reverting to frame number.", style=wx.ICON_ERROR | wx.OK)
                     self.plotparams['x_axis_display'] = 'frame'
-                    xdata = secm.frame_list
+                    xdata = secm.plot_frame_list
                 elif len(time) != len(ydata):
                     wx.CallAfter(wx.MessageBox, "Time data not available for this data set. Reverting to frame number.", style=wx.ICON_ERROR | wx.OK)
                     self.plotparams['x_axis_display'] = 'frame'
-                    xdata = secm.frame_list
+                    xdata = secm.plot_frame_list
                 else:
                     xdata = time
             else:
@@ -4589,9 +4589,9 @@ class SECPlotPanel(wx.Panel):
                         each.calc_line.set_label('I0')
 
             if self.plotparams['x_axis_display'] == 'frame':
-                each.line.set_xdata(each.frame_list)
+                each.line.set_xdata(each.plot_frame_list)
                 if each.calc_is_plotted:
-                    each.calc_line.set_xdata(each.frame_list)
+                    each.calc_line.set_xdata(each.plot_frame_list)
             elif self.plotparams['x_axis_display'] == 'time':
                 each.line.set_xdata(each.getTime())
                 if each.calc_is_plotted:
@@ -4720,9 +4720,9 @@ class SECPlotPanel(wx.Panel):
                 each.calc_line.set_ydata(np.zeros_like(each.line.get_ydata))
 
             if self.plotparams['x_axis_display'] == 'frame':
-                each.line.set_xdata(each.frame_list)
+                each.line.set_xdata(each.plot_frame_list)
                 if each.calc_is_plotted:
-                    each.calc_line.set_xdata(each.frame_list)
+                    each.calc_line.set_xdata(each.plot_frame_list)
             elif self.plotparams['x_axis_display'] == 'time':
                 each.line.set_xdata(each.getTime())
                 if each.calc_is_plotted:

SASM.py
@@ -740,6 +740,7 @@ class SECM:
             self._file_list=[sasm.getParameter('filename') for sasm in self._sasm_list]
 
         self.frame_list = self._frame_list_raw.copy()
+        self.plot_frame_list = np.arange(len(self.frame_list))
 
         self._scale_factor = 1
         self._offset_value = 0
@@ -892,6 +893,8 @@ class SECM:
 
         # print self.time
 
+        self.plot_frame_list = np.arange(len(self.frame_list))
+
         self._update()
 
 
@@ -929,22 +932,14 @@ class SECM:
             wx.CallAfter(wx.MessageBox, msg, "Invalid frame range", style = wx.ICON_ERROR | wx.OK)
             return sasms
 
-        elif len(np.where(self.frame_list == initial_frame)[0]) == 0:
+        elif len(np.where(self.plot_frame_list == initial_frame)[0]) == 0:
+            print np.where(self.plot_frame_list == initial_frame)
             msg = "To send data to the main plot, enter a valid frame range (initial frame not in data set)."
             wx.CallAfter(wx.MessageBox, msg, "Invalid frame range", style = wx.ICON_ERROR | wx.OK)
             return sasms
 
         else:
-            index1 = np.where(self.frame_list == initial_frame)[0][0]
-
-            if len(np.where(self.frame_list == final_frame)[0]) == 0:
-                index2 = len(self.frame_list)
-                print 'Warning: Final frame not in data set'
-            else:
-                index2 = np.where(self.frame_list == final_frame)[0][0]
-
-            sasms = self._sasm_list[index1 : index2+1]
-
+            sasms = self._sasm_list[initial_frame : final_frame+1]
             return sasms
 
     def getTime(self):